17ab096c9c2f423fc6b9a000ce9d13f4b47f1748
JackWu <androidzk@163.com>
2018-07-05 18:25:48 +0800

    no message

--- app/src/main/java/com/zoke/mytensorflow/MainActivity.java
+++ app/src/main/java/com/zoke/mytensorflow/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zoke.mytensorflow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+
 }
