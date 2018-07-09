6300a91adcb5925016bc4c8c20c8a66321ae16ed
JackWu <androidzk@163.com>
2018-07-09 17:51:41 +0800

    no message

---
+++ app/src/main/java/com/zoke/mytensorflow/FileUtil.java
@@ -0,0 +1,40 @@
+package com.zoke.mytensorflow;
+
+import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+
+public class FileUtil {
+
+    /**
+     * 删除或增加图片、视频等媒体资源文件时 通知系统更新媒体库，重新扫描
+     *
+     * @param filePath 文件路径，包括后缀
+     */
+    public static void notifyScanMediaFile(Context context, String filePath) {
+        if (context == null || TextUtils.isEmpty(filePath)) {
+            Log.e("FileUtil", "notifyScanMediaFile context is null or filePath is empty.");
+            return;
+        }
+
+        MediaScannerConnection.scanFile(context, new String[]{filePath}, null, new MediaScannerConnection.OnScanCompletedListener() {
+            @Override
+            public void onScanCompleted(String path, Uri uri) {
+                Log.i("FileUtil", "onScanCompleted");
+            }
+        });
+    }
+
+    public static File getPhotoCacheFolder() {
+        File cacheFolder = new File(Environment.getExternalStorageDirectory(), "TensorFlowPhotos");
+        if (!cacheFolder.exists()) {
+            cacheFolder.mkdirs();
+        }
+        return cacheFolder;
+    }
+}
